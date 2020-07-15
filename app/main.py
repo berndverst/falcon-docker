86ae469cd001b7958b28ffd5caea80106b2cdade
Bernd Verst <bverst@gmail.com>
2020-07-15 13:01:29 -0700

    Update test message.

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ class HelloWorldResource:
 
     def on_get(self, request, response):
 
-        response.media = ('Hello World from Falcon Python 3.6 app with' +
-                          ' Gunicorn running in a container.')
+        response.media = ('Hello World from Falcon Python with' +
+                          ' Gunicorn running in an Alpine Linux container.')
 
 
 app = falcon.API()
